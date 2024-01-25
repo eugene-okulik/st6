4df56c7341fad1ba1ad805c7ebfda813a12dc6c1
Ekaterina Klimova <klimova.ek.ser@yandex.by>
2024-01-25 18:01:39 +0300

    ekaterina_klimova_hometask3_calculator

---
+++ homework/ekaterina_klimova/task3_purchase.py
@@ -0,0 +1,14 @@
+﻿name = input(print("как вас зовут?"))
+
+purchase = input(print("что вы хотите купить?"))
+
+cost = float(input(print("сколько это стоит?")))
+
+cash = float(input(print("сколько у вас есть?")))
+
+saving = float(input(print("сколько можете отложить в месяц?")))
+
+print(f'Привет, {name}. На покупку {purchase} тебе не хватает {cash - cost}')
+print(f'Возможность совершения покупки: {(cash - cost) >= 0}')
+print(f'До покупки осталось {(cost - cash)/saving} месяцев')
+
