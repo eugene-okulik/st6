5d2229c91716f2e35679252f0ca97127b268efb6
Oleg <kov2905@icloud.com>
2024-01-27 14:18:41 +0200

    oleg:homeword_4 done

--- homework/kozlov_oleg/oleg_homework_4/task_1.py
+++ homework/kozlov_oleg/oleg_homework_4/task_1.py
@@ -1,8 +1,8 @@
 my_dict = {
-        'tuple': (1, 2, 3, 4, 5),
-        'list': [6, 7, 8, 9, 10],
-        'set': {11, 12, 13, 14, 15},
-        'dict': {'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5'}}
+    'tuple': (1, 2, 3, 4, 5),
+    'list': [6, 7, 8, 9, 10],
+    'set': {11, 12, 13, 14, 15},
+    'dict': {'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5'}}
 
 my_dict['list'].append(11)
 my_dict['list'].remove(7)
