9d6c130f33d2d7eecd733cf290ed598f392edeea
pavel-turonok <pasha11081998@yandex.com>
2024-01-27 18:09:45 +0300

    howework_3 done

---
+++ homework/Pavel_Turonok/homework_3/HW_3_Task_3.py
@@ -0,0 +1,11 @@
+data = [17, 33]
+average = sum(data) / len(data)
+print('Среднее арифмитическое чисел 17 и 33 =', average)
+
+
+import math
+geometric_mean = math.sqrt(data[0] * data[1])
+print('Среднее геометрическое чисел 17 и 33 = ', geometric_mean)
+
+
+
