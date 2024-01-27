e1d45e7abcbab05f80b3bb34cd38339d3cf01879
Oleg <kov2905@icloud.com>
2024-01-27 11:50:01 +0200

    oleg:homeword_4 done

--- homework/kozlov_oleg/oleg_homework_4/task_1.py
+++ homework/kozlov_oleg/oleg_homework_4/task_1.py
@@ -5,30 +5,17 @@ my_dict = {
     'dict' : {'one': '1', 'two': '2', 'three' : '3', 'four': '4', 'five' : '5'}
     }
 
-# Для того, что хранится под ключом ‘tuple’:выведите на экран последний элеме
 print(my_dict['tuple'][-1])
-
-# Для того, что хранится под ключом ‘list’:
-# добавьте в конец списка еще один элемент
 my_dict['list'].append(11)
 print(my_dict['list'])
-
-# удалите второй элемент списка
 my_dict['list'].remove(7)
 print(my_dict['list'])
-
-# Для того, что хранится под ключом ‘dict’:
-# добавьте элемент с ключом ('i am a tuple',) и любым значением
 my_dict['dict'].update({'i am a tuple': 1})
 print(my_dict['dict'])
-# удалите какой-нибудь элемент
 my_dict['dict'].pop('one')
 print(my_dict['dict'])
-
-# Для того, что хранится под ключом ‘set’:
-# добавьте новый элемент в множество
 my_dict['set'].add(0)
 print(my_dict['set'])
-# удалите элемент из множества
 my_dict['set'].remove(15)
 print(my_dict['set'])
+print(my_dict)
