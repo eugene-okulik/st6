09f074d5920bab65d3b3af0f0318d28727089e20
Oleg <kov2905@icloud.com>
2024-01-27 13:30:01 +0200

    oleg:homeword_4 done

--- homework/kozlov_oleg/oleg_homework_4/task_1.py
+++ homework/kozlov_oleg/oleg_homework_4/task_1.py
@@ -1,21 +1,19 @@
 my_dict = {
-    'tuple': (1, 2, 3, 4, 5),
-    'list': [6, 7, 8, 9, 10],
-    'set': {11, 12, 13, 14, 15},
-    'dict' : {'one': '1', 'two': '2', 'three' : '3', 'four': '4', 'five' : '5'}
-    }
+        'tuple': (1, 2, 3, 4, 5),
+        'list': [6, 7, 8, 9, 10],
+        'set': {11, 12, 13, 14, 15},
+        'dict': {'one': '1',
+                 'two': '2',
+                 'three': '3',
+                 'four': '4',
+                 'five': '5'
+                 }
+        }
 
-print(my_dict['tuple'][-1])
 my_dict['list'].append(11)
-print(my_dict['list'])
 my_dict['list'].remove(7)
-print(my_dict['list'])
 my_dict['dict'].update({'i am a tuple': 1})
-print(my_dict['dict'])
 my_dict['dict'].pop('one')
-print(my_dict['dict'])
 my_dict['set'].add(0)
-print(my_dict['set'])
 my_dict['set'].remove(15)
-print(my_dict['set'])
 print(my_dict)
