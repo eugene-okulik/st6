8a14f5e0d07ffef03d614f6c3ea94f4dfe92b6b0
ivan <i.telyuk@lerna.tech>
2024-01-30 17:29:52 +0300

    homework 4 done

---
+++ homework/ivan_teluk/homework_4/second_task.py
@@ -0,0 +1,7 @@
+text_1 = 'результат операции: 42'
+text_2 = 'результат операции: 514'
+text_3 = 'результат работы программы: 9'
+
+print(int(text_1[text_1.index(': ')+2:]) + 10)
+print(int(text_2[text_2.index(': ')+2:]) + 10)
+print(int(text_3[text_3.index(': ')+2:]) + 10)
