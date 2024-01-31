9e48ded7ffbaf607717a14efcf4ecc2089b3c6aa
Oleg <kov2905@icloud.com>
2024-01-31 18:31:06 +0200

    oleg:homeword_5 done

--- homework/kozlov_oleg/oleg_homework_5/task_1.py
+++ homework/kozlov_oleg/oleg_homework_5/task_1.py
@@ -1,5 +1,5 @@
-text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel.\n'
-'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel.\n'
+'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero')
 
 words = text.split()
 new_words = []
@@ -17,6 +17,7 @@ for word in words:
 new_text = " ".join(new_words)
 
 print(new_text)
+
 
 
 
