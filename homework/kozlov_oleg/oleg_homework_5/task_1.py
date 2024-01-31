78e55e0fc945d787614a77daa55556d3c8f0b028
Oleg <kov2905@icloud.com>
2024-01-31 18:25:37 +0200

    oleg:homeword_5 done

---
+++ homework/kozlov_oleg/oleg_homework_5/task_1.py
@@ -0,0 +1,18 @@
+text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+
+words = text.split()
+new_words = []
+
+for word in words:
+    if word[-1] in [",", "."]:
+        punctuation = word[-1]
+        word = word[:-1]
+    else:
+        punctuation = ""
+
+    new_word = word + "ing" + punctuation
+    new_words.append(new_word)
+
+new_text = " ".join(new_words)
+
+print(new_text)
