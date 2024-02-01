84449272caad24f9d3d3a5c0f8cf67dc3bb3e8d8
Aliaksandr <frombywithlove@gmail.com>
2024-02-01 19:04:15 +0300

    Homework 5 done

--- homework/aleksandr_tsvetkov/homework_5/Task1.py
+++ homework/aleksandr_tsvetkov/homework_5/Task1.py
@@ -5,7 +5,7 @@
 # слова идет запятая или точка, этот знак препинания должен идти после того же слова, но уже преобразованного
 
 
-text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, ' +
+text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, '
         'facilisis vitae semper at, dignissim vitae libero')
 split_from_text = text.split()
 new_text = []
