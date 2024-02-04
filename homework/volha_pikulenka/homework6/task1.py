ee122f5d7202b42e00f91a933a6f814e76e2cb38
Volha_Pikulenka <pikulenkoolga@gmail.com>
2024-02-04 22:28:55 +0300

    volha: fix lint

--- homework/volha_pikulenka/homework6/task1.py
+++ homework/volha_pikulenka/homework6/task1.py
@@ -1,4 +1,3 @@
-while(user_value := int(input('Guess a number from 1 to  15: ')) !=15):
+while (user_value := int(input('Guess a number from 1 to  15: ')) != 15):
     print('Wrong! Try again!')
 print('That\'s the number!')
-                
--- homework/volha_pikulenka/homework6/task2.py
+++ homework/volha_pikulenka/homework6/task2.py
@@ -1,4 +1,3 @@
 my_dict = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
 for key, value in my_dict.items():
     print(f'{key}' * int(value))
-    
