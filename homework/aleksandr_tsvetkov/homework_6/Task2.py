020804770bbeffc872c44799bf57ed78ce24b62d
Aliaksandr <frombywithlove@gmail.com>
2024-02-04 20:06:17 +0300

    homework 6 done

---
+++ homework/aleksandr_tsvetkov/homework_6/Task2.py
@@ -0,0 +1,8 @@
+# Дан такой словарь: words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
+# Выведите на экран каждый ключ столько раз сколько указано в значении.
+
+
+words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
+
+for key, value in words.items():
+    print(key * value)
