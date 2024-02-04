3a5eea694581311b10c7567d7f7253545b0ee2ec
Volha_Pikulenka <pikulenkoolga@gmail.com>
2024-02-04 22:24:25 +0300

    volha: homework5 - task3

---
+++ homework/volha_pikulenka/homework6/task3.py
@@ -0,0 +1,14 @@
+a = 'результат операции: 42'
+b = 'результат операции: 54'
+c = 'результат работы программы: 209'
+d = 'результат работы программы: 2'
+
+def plus_ten(num):
+    num_ind = num.index(':') + 2
+    num_plus_ten = int(num[num_ind:]) + 10
+    print(num_plus_ten)
+
+plus_ten(a)
+plus_ten(b)
+plus_ten(c)
+plus_ten(d)
