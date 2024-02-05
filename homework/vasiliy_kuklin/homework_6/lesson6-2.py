69fff2c4d36528e71d5bc5741a50273693de7fee
brozzz1999 <vasiliy.kuklin@gmail.com>
2024-02-05 13:09:41 +0300

    vasiliy_kuklin_hw_6_fixing_bugs_in_lint

--- homework/vasiliy_kuklin/homework_6/lesson6-2.py
+++ homework/vasiliy_kuklin/homework_6/lesson6-2.py
@@ -1,7 +1,7 @@
 # Задание №2
 words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
 
-for k,v in words.items():
+for k, v in words.items():
     for i in range(v):
         print(k, end='')
     print('')
--- homework/vasiliy_kuklin/homework_6/lesson6-3.py
+++ homework/vasiliy_kuklin/homework_6/lesson6-3.py
@@ -4,11 +4,13 @@ second = "результат операции: 54"
 third = "результат работы программы: 209"
 fourth = "результат: 2"
 
+
 def calc_num_in_strings(texts):
     text = texts.split(': ')
     result = (int(text[1]) + 10)
     print(result)
     return result
+
 
 calc_num_in_strings(first)
 calc_num_in_strings(second)
