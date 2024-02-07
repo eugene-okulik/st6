ae604d74ea2335af1a0323d36548cb6603a3ea03
brozzz1999 <vasiliy.kuklin@gmail.com>
2024-02-07 21:27:27 +0300

    comments corrected

--- homework/vasiliy_kuklin/homework_5/lesson5-1.py
+++ homework/vasiliy_kuklin/homework_5/lesson5-1.py
@@ -1,8 +1,8 @@
 # Задание №1
-my_string = \
-    ("Etiam tincidunt neque erat, quis molestie enim imperdiet vel."
-     " Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
-     )
+my_string = (
+        "Etiam tincidunt neque erat, quis molestie enim imperdiet vel."
+        " Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+)
 
 list_from_my_string = my_string.split()
 print(list_from_my_string)
@@ -14,4 +14,5 @@ for word in list_from_my_string:
         word = (word[:-1] + 'ing' + '.')
     else:
         word = word + 'ing'
-    print(word)
+    new_list = word.join([word])
+    print(new_list, end=' ')
