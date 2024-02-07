77540563bca43a0b331dc3d16039fe5b71726dc9
brozzz1999 <vasiliy.kuklin@gmail.com>
2024-02-07 22:26:34 +0300

    vasiliy_kuklin_hw_4_fixing comments

--- homework/vasiliy_kuklin/homework_4/lesson4.py
+++ homework/vasiliy_kuklin/homework_4/lesson4.py
@@ -1,15 +1,15 @@
-my_dict = \
-    {'tuple': (1, 2, 3, 4, 5),
-     'list': [1, "paris", None, 2.5, 777],
-     'dict': {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5},
-     'set': {1, "sport", "solo", False, 5}
-     }
+my_dict = {
+    'tuple': (1, 2, 3, 4, 5),
+    'list': [1, "paris", None, 2.5, 777],
+    'dict': {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5},
+    'set': {1, "sport", "solo", False, 5}
+}
 
 # Задание 1
 print(my_dict['tuple'][-1])
 my_dict['list'].append(888)
 my_dict['list'].pop(1)
-my_dict['dict'].update({'i am a tuple': 900})
+my_dict['dict'].update({('i am a tuple',): 900})
 my_dict['dict'].pop('two')
 my_dict['set'].add(9)
 my_dict['set'].pop()
