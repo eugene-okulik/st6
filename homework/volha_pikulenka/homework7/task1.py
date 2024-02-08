f681b4b0363b78f4c0a36e8cecccced9c0c39f4c
Volha_Pikulenka <pikulenkoolga@gmail.com>
2024-02-08 21:02:56 +0300

    fibonacci generator

--- homework/volha_pikulenka/homework7/task1.py
+++ homework/volha_pikulenka/homework7/task1.py
@@ -2,12 +2,12 @@ def fibonacci_gen():
     a, b = 0, 1
     while True:
         yield a
-        a,b = b, a+b
+        a, b = b, a + b
 
 
 fib = fibonacci_gen()
 fib_int = []
-for _ in range(100_000):    
+for _ in range(100_000):
     fib_int.append(next(fib))
 
 print(fib_int[4])
