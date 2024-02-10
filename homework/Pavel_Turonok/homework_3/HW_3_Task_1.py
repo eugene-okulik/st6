44786cf3a136077af95b34cb06456fcd395df20d
pavel-turonok <pasha11081998@yandex.com>
2024-02-10 19:57:18 +0300

    homework 3 fix lint

--- homework/Pavel_Turonok/homework_3/HW_3_Task_1.py
+++ homework/Pavel_Turonok/homework_3/HW_3_Task_1.py
@@ -2,5 +2,5 @@ a = 5
 b = 25
 
 print('Сумма a + b =', a + b)
-print('Разность a - b =',a - b)
-print('Произведение a * b =',a * b)
+print('Разность a - b =', a - b)
+print('Произведение a * b =', a * b)
