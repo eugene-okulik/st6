790ce3e57f29f7c22e447d9137e633bfe98589c2
Volha_Pikulenka <pikulenkoolga@gmail.com>
2024-02-11 12:05:47 +0300

    volha: homewok8

--- homework/volha_pikulenka/homework8/task1.py
+++ homework/volha_pikulenka/homework8/task1.py
@@ -1,8 +1,8 @@
 import datetime
 
 init_date = 'Jan 15, 2023 - 12:05:33'
-date_in_py = datetime.datetime.strptime(init_date, '%b %d, %Y - %H:%M:%S')
-print(datetime.datetime.strptime(init_date, '%b %d, %Y - %H:%M:%S'))
+py_date = datetime.datetime.strptime(init_date, '%b %d, %Y - %H:%M:%S')
+print(py_date)
 
-req_format = date_in_py.strftime('%d.%m.%Y, %H:%M')
-print(req_format)
+req_date_format = py_date.strftime('%d.%m.%Y, %H:%M')
+print(req_date_format)
