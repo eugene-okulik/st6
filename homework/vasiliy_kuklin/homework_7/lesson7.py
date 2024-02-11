8d28f2996d18ae0dae60a53c0a0fcb8bbb8b540d
brozzz1999 <vasiliy.kuklin@gmail.com>
2024-02-11 14:34:19 +0300

    vasiliy_kuklin_hw_7_fix_comment_bugs

--- homework/vasiliy_kuklin/homework_7/lesson7.py
+++ homework/vasiliy_kuklin/homework_7/lesson7.py
@@ -1,14 +1,12 @@
 # Ряд Фибаначчи в генераторе
-def gen_fiba():
-    list_fiba = [0, 1, 1, ]
-    for i in range(2, 100001):
-        fiba = list_fiba[-1] + list_fiba[-2]
-        list_fiba.append(fiba)
-    yield list_fiba
-    print(list_fiba[5])
-    print(list_fiba[200])
-    print(list_fiba[1000])
-    print(list_fiba[100000])
+def gen_fiba(num):
+    a, b = 0, 1
+    for _ in range(num):
+        yield a
+        a, b = b, a + b
 
 
-print(list(gen_fiba()))
+print(list(gen_fiba(5))[-1])
+print(list(gen_fiba(200))[-1])
+print(list(gen_fiba(1000))[-1])
+print(list(gen_fiba(100000))[-1])
