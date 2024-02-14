215111fa0b9f47287f3b58771f7d060f4878e989
Volha_Pikulenka <pikulenkoolga@gmail.com>
2024-02-14 19:19:45 +0300

    volha: homework9

---
+++ homework/volha_pikulenka/homework9/task1.py
@@ -0,0 +1,12 @@
+def finish_me(func):
+    def wrapper(*args, **kwargs):
+        func(*args, **kwargs)
+        print('finished')
+    return wrapper
+
+
+@finish_me
+def example(text):
+    print(text)
+    
+example('print me')
