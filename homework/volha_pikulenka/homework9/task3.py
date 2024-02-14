e027185bb0139e4dba29bc1344a96e68730e2bb6
Volha_Pikulenka <pikulenkoolga@gmail.com>
2024-02-14 21:16:01 +0300

    volha:homework9

--- homework/volha_pikulenka/homework9/task3.py
+++ homework/volha_pikulenka/homework9/task3.py
@@ -1,20 +1,20 @@
 def calc_decorator(func):
     def wrapper(*args, **kwargs):
 
-         if args[0] < 0 or args[1] < 0: 
-              return print(args[0] * args[1])
-         # это условие должно быть входом, иначе оно всегда игнорируется
+        if args[0] < 0 or args[1] < 0: 
+            return print(args[0] * args[1])
+        # это условие должно быть входом, иначе оно всегда игнорируется
 
-         elif args[0] == args[1]:
-               return print(args[0] + args[1])
+        elif args[0] == args[1]:
+            return print(args[0] + args[1])
 
-         elif args[0] > args[1]:
-              return print(args[0] - args[1])
+        elif args[0] > args[1]:
+            return print(args[0] - args[1])
 
-         elif args[1] > args[0]:
-              return print(args[0] / args[1])
+        elif args[1] > args[0]:
+            return print(args[0] / args[1])
 
-         print(func(*args, **kwargs))
+        print(func(*args, **kwargs))
 
     return wrapper
 
@@ -25,18 +25,18 @@ oper = input('\nEnter operation type: ')
 
 @calc_decorator
 def calc(number1, number2, operation):
-     if operation == '+':
-          sum = number1 + number2
-          return sum
-     elif operation == '-':
-          substr = number1 - number2
-          return substr
-     elif operation == '/':
-          division = number1 / number2
-          return division
-     elif operation == '*':
-          mult = number1 * number2
-          return mult
+    if operation == '+':
+        sum = number1 + number2
+        return sum
+    elif operation == '-':
+        substr = number1 - number2
+        return substr
+    elif operation == '/':
+        division = number1 / number2
+        return division
+    elif operation == '*':
+        mult = number1 * number2
+        return mult
 
 
 calc(num1, num2, oper)
