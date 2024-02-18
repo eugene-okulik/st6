0d7ce07345445ac67422a8de239293f0e1e24e8b
Volha_Pikulenka <pikulenkoolga@gmail.com>
2024-02-18 11:20:17 +0300

    volha:homework9

---
+++ homework/volha_pikulenka/homework9/task2-2.py
@@ -0,0 +1,12 @@
+def repeat_me(func):
+    def wrapper(*args, count=''):
+        for _ in range(count):
+            func(*args)
+    return wrapper
+
+@repeat_me
+def example(text):
+    print(text)
+
+
+example('print me', count=2)
