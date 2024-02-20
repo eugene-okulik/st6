519618e92c5527defbde0208b729b1d6f09e7467
brozzz1999 <vasiliy.kuklin@gmail.com>
2024-02-20 12:34:22 +0300

    vasiliy_kuklin_hw_9_fix_lint

--- homework/vasiliy_kuklin/homework_9/lesson9-2.py
+++ homework/vasiliy_kuklin/homework_9/lesson9-2.py
@@ -1,10 +1,11 @@
 # Задание №2
 def repeater(func):
     def wrapper(*args, **kwargs):
-            for key, value in kwargs.items():
-                if value in kwargs.values():
-                    for i in range(value):
-                        func(*args)
+        for key, value in kwargs.items():
+            if value in kwargs.values():
+                for i in range(value):
+                    func(*args)
+
     return wrapper
 
 
