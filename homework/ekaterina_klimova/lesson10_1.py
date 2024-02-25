f8117fde12736ef40c657d9e890119cff175b4fd
Ekaterina Klimova <klimova.ek.ser@yandex.by>
2024-02-25 17:27:01 +0300

    ekaterina_klimova_hw10_4

--- homework/ekaterina_klimova/lesson10_1.py
+++ homework/ekaterina_klimova/lesson10_1.py
@@ -1,5 +1,5 @@
 class Book:
-    def __init__(self, title, author, pages, ISBN = "isbn", mat = "бумага", isText = "Да", isBooked = False):
+    def __init__(self, title, author, pages, ISBN="isbn", mat="бумага", isText="Да", isBooked=False):
         self.mat = mat
         self.isText = isText
         self.title = title
@@ -9,15 +9,15 @@ class Book:
         self.isBooked = isBooked
 
 
-def print_info(book):
+def print_info(b):
     if book.isBooked:
         res_status = "зарезервирована"
     else:
         res_status = ""
-    print(f'Название: {book.title}, Автор: {book.author}, Количество страниц: {book.pages}, Материал: {book.mat}, {res_status}')
+    print(f'Название: {b.title}, Автор: {b.author}, Количество страниц: {b.pages}, Материал: {b.mat}, {res_status}')
 
 
-book1 = Book("Идиот", "Ф.М.Достоевский", 500, isBooked = True)
+book1 = Book("Идиот", "Ф.М.Достоевский", 500, isBooked=True)
 book2 = Book("Ася", "И.С.Тургеньев", 680)
 book3 = Book("Анна Каренина", "Л.Н.Толстой", 400)
 book4 = Book("Мцыри", "М.Ю.Лермонтов", 300)
--- homework/ekaterina_klimova/lesson10_2.py
+++ homework/ekaterina_klimova/lesson10_2.py
@@ -1,5 +1,5 @@
 class Book:
-    def __init__(self, title, author, pages, ISBN = "isbn", mat = "бумага", isText = "Да", isBooked = False):
+    def __init__(self, title, author, pages, ISBN="isbn", mat="бумага", isText="Да", isBooked=False):
         self.mat = mat
         self.isText = isText
         self.title = title
@@ -9,15 +9,15 @@ class Book:
         self.isBooked = isBooked
 
 
-def print_info(book):
+def print_info(b):
     if book.isBooked:
         res_status = "зарезервирована"
     else:
         res_status = ""
-    print(f'Название: {book.title}, Автор: {book.author}, Количество страниц: {book.pages}, Материал: {book.mat}, {res_status}')
+    print(f'Название: {b.title}, Автор: {b.author}, Количество страниц: {b.pages}, Материал: {b.mat}, {res_status}')
 
 
-book1 = Book("Идиот", "Ф.М.Достоевский", 500, isBooked = True)
+book1 = Book("Идиот", "Ф.М.Достоевский", 500, isBooked=True)
 book2 = Book("Ася", "И.С.Тургеньев", 680)
 book3 = Book("Анна Каренина", "Л.Н.Толстой", 400)
 book4 = Book("Мцыри", "М.Ю.Лермонтов", 300)
@@ -31,23 +31,23 @@ print_info(book5)
 
 
 class SchoolBook(Book):
-    def __init__(self, subject, grade, title, author, pages, ISBN="isbn", mat="бумага", isText="Да", isBooked=False, task = True):
+    def __init__(self, subject, grade, title, author, pages, ISBN="isbn", mat="бумага", isText="Да", isBooked=False, task=True):
         super().__init__(title, author, pages, ISBN, mat, isText, isBooked)
         self.subject = subject
         self.grade = grade
         self.task = task
 
 
-book6 = SchoolBook("Алгебра", 9, "Математика", "Иванов", 500, isBooked = True)
+book6 = SchoolBook("Алгебра", 9, "Математика", "Иванов", 500, isBooked=True)
 book7 = SchoolBook("География", 9, "География", "Сидоров", 300)
 
 
-def print_info(schB):
-    if schB.isBooked:
+def print_info(sB):
+    if sB.isBooked:
         res_status = "зарезервирована"
     else:
         res_status = ""
-    print(f'Название: {schB.title}, Автор: {schB.author}, Количество страниц: {schB.pages}, Материал: {schB.mat}, Предмет: {schB.subject}, Класс: {schB.grade}, {res_status}')
+    print(f'Название: {sB.title}, Автор: {sB.author}, Количество страниц: {sB.pages}, Материал: {sB.mat}, Предмет: {sB.subject}, Класс: {sB.grade}, {res_status}')
 
 print_info(book6)
 print_info(book7)
