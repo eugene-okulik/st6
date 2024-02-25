3cf47f07be638da963d30dc5ba4ea23f15a19aa3
pavel-turonok <pasha11081998@yandex.com>
2024-02-25 17:22:33 +0300

    homework_5 done

---
+++ homework/Pavel_Turonok/homework_5/HW_5_1.py
@@ -0,0 +1,11 @@
+text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero')
+
+split_text = text.split()
+new_text = []
+
+for i in split_text:
+    if i[-1] == '.' or i[-1] == ',':
+        new_text.append(i[:-1] + 'ing' + i[-1])
+    else:
+        new_text.append(i + 'ing')
+print(' '.join(new_text))
