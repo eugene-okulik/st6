a062ec4ec344b7825cc9f677911363e781f2ad92
Volha_Pikulenka <pikulenkoolga@gmail.com>
2024-02-26 20:38:21 +0300

    volha: homework 11

--- homework/volha_pikulenka/homework11/Flowers.py
+++ homework/volha_pikulenka/homework11/Flowers.py
@@ -6,11 +6,11 @@ class Flowers:
         self.avr_lifetime_in_days = avr_lifetime_in_days
 
     def flower_info(self):
-        return (self.name,self.price,self.color, self.avr_lifetime_in_days)
-              
+        return (self.name, self.price, self.color, self.avr_lifetime_in_days)
+
     def flower_desc(self):
         print(f'Name: {self.name.title()}, Price: {self.price}, Color: {self.color}, '
-            f'Lifetime: {self.avr_lifetime_in_days}')
+              f'Lifetime: {self.avr_lifetime_in_days}')
 
 
 red_rose = Flowers('rose', 20, 'red', 10)
@@ -21,14 +21,14 @@ daffodils = Flowers('daffodils', 30, 'bright-yellow', 15)
 
 class Houseplants(Flowers):
     def __init__(self, name: str, price: int, color: str, avr_lifetime_in_days: int,
-                is_houseplant=True):
+                 is_houseplant=True):
         super().__init__(name, price, color, avr_lifetime_in_days)
         self.is_houseplant = is_houseplant
 
     def flower_desc(self):
         print(f'Name: {self.name.title()}, Price: {self.price}, '
-            + f'Color: {self.color}, Lifetime: {self.avr_lifetime_in_days},'
-            + f'Hoseplant: {self.is_houseplant}')
+             + f'Color: {self.color}, Lifetime: {self.avr_lifetime_in_days}, '
+             + f'Hoseplant: {self.is_houseplant}')
 
 
 viola = Houseplants('viola', 44, 'violet', 365, True)
