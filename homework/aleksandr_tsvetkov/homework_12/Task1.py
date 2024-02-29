dc7390a61880068693ca337d852492742c30dff8
Aliaksandr <frombywithlove@gmail.com>
2024-03-01 00:45:33 +0300

    homework 12 done

--- homework/aleksandr_tsvetkov/homework_12/Task1.py
+++ homework/aleksandr_tsvetkov/homework_12/Task1.py
@@ -2,12 +2,13 @@ import datetime
 
 
 def date_conversion():
-    try:
-        user_date = input('Введите дату: ')
-        python_date = datetime.datetime.strptime(user_date, '%d.%m.%Y')
-        return f'Питоновский формат даты: {python_date}'
-    except ValueError:
-        return 'Неверный формат даты. Правильный формат даты: (дд/мм/гггг), например 29.09.2024'
+    while True:
+        try:
+            user_date = input('Введите дату: ')
+            python_date = datetime.datetime.strptime(user_date, '%d.%m.%Y')
+            return f'Питоновский формат даты: {python_date}'
+        except ValueError:
+            print('Неверный формат даты. Правильный формат даты: (дд/мм/гггг), например 29.09.2024')
 
 
 print(date_conversion())
