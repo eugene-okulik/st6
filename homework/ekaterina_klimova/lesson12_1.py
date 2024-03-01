d45a67508c0d076467f2aa426e2fca360acddfff
Ekaterina Klimova <klimova.ek.ser@yandex.by>
2024-03-01 15:12:06 +0300

    ekaterina_klimova_hw12

---
+++ homework/ekaterina_klimova/lesson12_1.py
@@ -0,0 +1,12 @@
+﻿from datetime import datetime
+
+while True:
+    date_t = input("Введите дату в формате (ГГГГ-ММ-ДД):")
+
+
+    try:
+        date_obj = datetime.strptime(date_t, '%Y-%m-%d')
+        print("Спасибо! Дата введена корректно: ", date_obj)
+        break
+    except:
+        print("Введите, пожалуйста, дату в формате (ГГГГ-ММ-ДД)")
