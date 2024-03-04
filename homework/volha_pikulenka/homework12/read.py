c1a0a70e304d569a83cc19488467670e92f9f30b
Volha_Pikulenka <pikulenkoolga@gmail.com>
2024-03-04 09:30:33 +0300

    volha: homework 12 new

---
+++ homework/volha_pikulenka/homework12/read.py
@@ -0,0 +1,17 @@
+import os
+
+
+repo_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+print(repo_path)
+path_to_file = os.path.join(repo_path, 'eugene_okulik', 'Lesson_12', 'data.txt')
+print(path_to_file)
+
+try:
+    with open(path_to_file, 'r', encoding='utf-8') as opened_file:
+        data = opened_file.read()
+        print(data)
+        for letter in data:
+            if letter.istitle():
+                print(letter)
+except (FileNotFoundError) as err:
+    print(err)
