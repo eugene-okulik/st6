a6f8f483115d604f30a00297ea3b649c1f8a6645
brozzz1999 <vasiliy.kuklin@gmail.com>
2024-03-16 21:18:51 +0300

    vasiliy_kuklin_hw_14_with_selects

--- homework/vasiliy_kuklin/homework_14/lesson14.sql
+++ homework/vasiliy_kuklin/homework_14/lesson14.sql
@@ -48,3 +48,27 @@ INSERT INTO marks (value, lesson_id, student_id)
 VALUES (5,
 (SELECT id FROM lessons WHERE title = 'Zanyatie two' AND subject_id = (SELECT id FROM subjets WHERE title = 'Arganaftika')),
 (SELECT id FROM students WHERE name = 'Mark'))
+
+-- Получите информацию из базы данных:
+-- Все оценки студента
+    SELECT value FROM marks WHERE student_id = (SELECT id FROM students WHERE name ='Mark')
+
+-- Все книги, которые находятся у студента
+    SELECT title FROM books WHERE taken_by_student_id = (SELECT id FROM students WHERE name ='Mark')
+
+-- Для вашего студента выведите всё, что о нем есть в базе:
+-- группа, книги, оценки с названиями занятий и предметов
+-- (всё одним запросом с использованием Join)
+SELECT s.name as student, g.title as gruppa, b.title as kniga, m.value as ocenka, l.title as zanyatie, sb.title as predmet
+FROM students  s
+LEFT JOIN `groups` g
+ON s.group_id =g.id
+JOIN books b
+ON s.id = b.taken_by_student_id
+JOIN marks m
+ON s.id = m.student_id
+JOIN lessons l
+ON l.id = m.lesson_id
+JOIN subjets sb
+ON sb.id = l.subject_id
+WHERE s.name = 'Mark'
