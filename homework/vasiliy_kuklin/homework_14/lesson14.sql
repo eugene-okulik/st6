d2ba7f643169932b8a2124222a59e4d289c16a26
brozzz1999 <vasiliy.kuklin@gmail.com>
2024-03-19 20:03:13 +0300

    vasiliy_kuklin_hw_14_fix_comments

--- homework/vasiliy_kuklin/homework_14/lesson14.sql
+++ homework/vasiliy_kuklin/homework_14/lesson14.sql
@@ -1,60 +1,63 @@
 -- Создайте студента (student)
-INSERT INTO students(name, second_name) VALUES ('Mark','Twen')
+INSERT INTO students(name, second_name) VALUES ('Ioan','Grozny')
 
 -- Создайте несколько книг (books) и укажите, что ваш созданный студент взял их
-INSERT  INTO books (title, taken_by_student_id) values ('Skazki', 121)
+INSERT  INTO books (title, taken_by_student_id)
+VALUES ('Kniga_odin', (SELECT id FROM students WHERE name = 'Ioan' AND second_name = 'Grozny'))
 
-INSERT  INTO books (title, taken_by_student_id) values ('Shutki', 121)
+INSERT  INTO books (title, taken_by_student_id)
+VALUES ('Kniga_dva', (SELECT id FROM students WHERE name = 'Ioan' AND second_name = 'Grozny'))
 
-INSERT  INTO books (title, taken_by_student_id) values ('Pribautki', 121)
+INSERT  INTO books (title, taken_by_student_id)
+VALUES ('Kniga_tri', (SELECT id FROM students WHERE name = 'Ioan' AND second_name = 'Grozny'))
 
 -- Создайте группу (group) и определите своего студента туда
 INSERT INTO `groups` (title) values ('Aria')
 
-UPDATE students  SET group_id  = (SELECT id FROM `groups` WHERE title = 'Aria' ) WHERE name = 'Mark'
+UPDATE students  SET group_id  = (SELECT id FROM `groups` WHERE title = 'Aria' ) WHERE name = 'Ioan' AND second_name = 'Grozny'
 
 -- Создайте несколько учебных предметов (subjects)
-INSERT INTO subjets (title) VALUES ('Geodezika')
+INSERT INTO subjets (title) VALUES ('Histori_Russia')
 
-INSERT INTO subjets (title) VALUES ('Arganaftika')
+INSERT INTO subjets (title) VALUES ('Geographi_Russia')
 
 -- Создайте по два занятия для каждого предмета (lessons)
-INSERT INTO lessons (title, subject_id) VALUES ('Zanyatie one', (SELECT id FROM subjets WHERE title = 'Geodezika'))
+INSERT INTO lessons (title, subject_id) VALUES ('Zanyatie 1', (SELECT id FROM subjets WHERE title = 'Histori_Russia'))
 
-INSERT INTO lessons (title, subject_id) VALUES ('Zanyatie two', (SELECT id FROM subjets WHERE title = 'Geodezika'))
+INSERT INTO lessons (title, subject_id) VALUES ('Zanyatie 2', (SELECT id FROM subjets WHERE title = 'Histori_Russia'))
 
-INSERT INTO lessons (title, subject_id) VALUES ('Zanyatie one', (SELECT id FROM subjets WHERE title = 'Arganaftika'))
+INSERT INTO lessons (title, subject_id) VALUES ('Zanyatie 1', (SELECT id FROM subjets WHERE title = 'Geographi_Russia'))
 
-INSERT INTO lessons (title, subject_id) VALUES ('Zanyatie two', (SELECT id FROM subjets WHERE title = 'Arganaftika'))
+INSERT INTO lessons (title, subject_id) VALUES ('Zanyatie 2', (SELECT id FROM subjets WHERE title = 'Geographi_Russia'))
 
 -- Поставьте своему студенту оценки (marks) для всех созданных вами занятий
 
 INSERT INTO marks (value, lesson_id, student_id)
 VALUES (5,
-(SELECT id FROM lessons WHERE title = 'Zanyatie one' AND subject_id = (SELECT id FROM subjets WHERE title = 'Geodezika')),
-(SELECT id FROM students WHERE name = 'Mark'))
+(SELECT id FROM lessons WHERE title = 'Zanyatie 1' AND subject_id = (SELECT id FROM subjets WHERE title = 'Histori_Russia')),
+(SELECT id FROM students WHERE name = 'Ioan' AND second_name = 'Grozny'))
 
 INSERT INTO marks (value, lesson_id, student_id)
 VALUES (5,
-(SELECT id FROM lessons WHERE title = 'Zanyatie two' AND subject_id = (SELECT id FROM subjets WHERE title = 'Geodezika')),
-(SELECT id FROM students WHERE name = 'Mark'))
+(SELECT id FROM lessons WHERE title = 'Zanyatie 2' AND subject_id = (SELECT id FROM subjets WHERE title = 'Histori_Russia')),
+(SELECT id FROM students WHERE name = 'Ioan' AND second_name = 'Grozny'))
 
 INSERT INTO marks (value, lesson_id, student_id)
 VALUES (5,
-(SELECT id FROM lessons WHERE title = 'Zanyatie one' AND subject_id = (SELECT id FROM subjets WHERE title = 'Arganaftika')),
-(SELECT id FROM students WHERE name = 'Mark'))
+(SELECT id FROM lessons WHERE title = 'Zanyatie 1' AND subject_id = (SELECT id FROM subjets WHERE title = 'Geographi_Russia')),
+(SELECT id FROM students WHERE name = 'Ioan' AND second_name = 'Grozny'))
 
 INSERT INTO marks (value, lesson_id, student_id)
 VALUES (5,
-(SELECT id FROM lessons WHERE title = 'Zanyatie two' AND subject_id = (SELECT id FROM subjets WHERE title = 'Arganaftika')),
-(SELECT id FROM students WHERE name = 'Mark'))
+(SELECT id FROM lessons WHERE title = 'Zanyatie 2' AND subject_id = (SELECT id FROM subjets WHERE title = 'Geographi_Russia')),
+(SELECT id FROM students WHERE name = 'Ioan' AND second_name = 'Grozny'))
 
 -- Получите информацию из базы данных:
 -- Все оценки студента
-    SELECT value FROM marks WHERE student_id = (SELECT id FROM students WHERE name ='Mark')
+    SELECT value FROM marks WHERE student_id = (SELECT id FROM students WHERE name = 'Ioan' AND second_name = 'Grozny')
 
 -- Все книги, которые находятся у студента
-    SELECT title FROM books WHERE taken_by_student_id = (SELECT id FROM students WHERE name ='Mark')
+    SELECT title FROM books WHERE taken_by_student_id = (SELECT id FROM students WHERE name = 'Ioan' AND second_name = 'Grozny')
 
 -- Для вашего студента выведите всё, что о нем есть в базе:
 -- группа, книги, оценки с названиями занятий и предметов
@@ -71,4 +74,5 @@ JOIN lessons l
 ON l.id = m.lesson_id
 JOIN subjets sb
 ON sb.id = l.subject_id
-WHERE s.name = 'Mark'
+WHERE s.name = 'Ioan'
+AND s.second_name = 'Grozny'
