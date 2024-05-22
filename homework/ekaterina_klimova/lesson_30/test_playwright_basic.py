bd1f0119e48df25a3265f043e50b32fb687975ee
Ekaterina Klimova <klimova.ek.ser@yandex.by>
2024-05-22 08:40:35 +0300

    ekaterina_klimova_HW30

---
+++ homework/ekaterina_klimova/lesson_30/test_playwright_basic.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import Page
+
+
+def test_authentication(page: Page):
+    page.goto('https://the-internet.herokuapp.com/')
+    form_auth = page.get_by_role('link', name='Form Authentication')
+    user_name = page.get_by_role('textbox').first
+    passw = page.get_by_role('textbox').nth(1)
+    login_button = page.get_by_role('button')
+    form_auth.click()
+    user_name.fill('Kate')
+    passw.fill('QWerty12345^')
+    login_button.click()
+
+
+
