af418057a238f167ed2c64a11551c86dbb466772
Ekaterina Klimova <klimova.ek.ser@yandex.by>
2024-08-09 09:02:23 +0300

    ekaterina_klimova_HW37

--- test_UI_eklimova_selenium/conftest.py
+++ test_UI_eklimova_selenium/conftest.py
@@ -1,21 +1,4 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from test_UI_eklimova_selenium.pages.create_new_account_page import CreateNewAccount
-from test_UI_eklimova_selenium.pages.eco_frienfly_page import EcoFriendlyPage
-from test_UI_eklimova_selenium.pages.sale_page import SalePage
-import pytest
-import json
 
-
-@pytest.fixture()
-def driver():
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome()
-    yield driver
-    driver.quit()
 
 
 @pytest.fixture()
